87a8c7aefe4a1c9feca9d19fe2e0fd911ecde513
Embyr W <embyrwilliams@gmail.com>
2022-07-19 21:05:28 -0400

    at JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a+b}
+      </p>
     </div>
-  );
+  )
 }
 
 export default App;
