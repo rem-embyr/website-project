8187d948cff62b48993dea2000a213794938e7de
Embyr W <embyrwilliams@gmail.com>
2022-07-20 20:01:41 -0400

    part2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
+  )
+}
+
+const App = () => {
+  const name = 'Matthew'
+  const age = 20
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name={name} age = {age}/>
+      <Hello name="Embyr" age={10+9}/>
+    </>
   )
 }
 
